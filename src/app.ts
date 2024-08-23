ca6fcc765166787dd6fc14cad0282aca7b5cb534
Paul Ehrling <ghostlest330@gmail.com>
2024-08-23 10:33:35 +0200

    chore: Add body-parser middleware for parsing request bodies

--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,6 @@
 import express from 'express';
 import dotenv from 'dotenv';
+import bodyParser from 'body-parser';
 dotenv.config();
 
 // Helper scripts
@@ -8,6 +9,14 @@ import checkEnvVariable from './helper/envVariable';
 checkEnvVariable();
 
 const app = express();
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+// Routes
+import AuthRoutes from './routes/auth.routes';
+
+app.use(AuthRoutes);
 
 app.listen(process.env.PORT, () => {
   console.log(`Server is running on ${process.env.PORT}`);
