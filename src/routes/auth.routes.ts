967d547c4c6cd723f573fafaa8ae90cbf15b8418
Paul Ehrling <ghostlest330@gmail.com>
2024-08-23 10:40:47 +0200

    Add Middleware to check if user is logged in

--- src/routes/auth.routes.ts
+++ src/routes/auth.routes.ts
@@ -1,8 +1,11 @@
 import { Router } from 'express';
 
 import AuthController from '../controllers/auth.controller';
+import AuthenticateAPI from '../middlewares/authenticate.middleware';
 
 const router = Router();
+
+router.use(AuthenticateAPI.authenticate);
 
 router.post('/auth', AuthController.generateToken);
 
